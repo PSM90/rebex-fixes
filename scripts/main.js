b43b0f9647dd1856aa54ee01d6d367ed520508d3
PSM90 <108842920+PSM90@users.noreply.github.com>
2024-10-23 01:20:35 +0200

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,102 +1,28 @@
 // main.js
 
-import { CompendiumUtilities, SpellConcentrationFixer } from './utilities.js';
+import { showFixSelectionDialog } from './helpers.js';
 
-function registerSettings() {
-    game.settings.registerMenu("rebex-fixes", "rebexFixesMenu", {
+Hooks.once('init', async function () {
+    console.log("Rebex Fixes | Inizializzazione del modulo...");
+});
+
+Hooks.once('ready', async function () {
+    console.log("Rebex Fixes | Modulo pronto!");
+
+    // Aggiunta dell'opzione nelle impostazioni per aprire la finestra di dialogo di selezione
+    game.settings.registerMenu("rebex-fixes", "fixSelection", {
         name: "Rebex Fixes",
-        label: "Rebex Fixes",
-        hint: "Apri l'interfaccia per eseguire fix su compendi e schede.",
+        label: "Apri finestra di Fix",
+        hint: "Permette di selezionare un target e applicare i fix di oggetti e incantesimi.",
         icon: "fas fa-wrench",
-        type: RebexFixesApp,
+        type: showFixSelectionDialog,
         restricted: true
     });
 
-    game.settings.register("rebex-fixes", "dummySetting", {
-        name: "Rebex Fixes Setting",
-        scope: "world",
-        config: false,
-        default: "",
-        type: String
-    });
-}
-
-Hooks.once('init', () => {
-    console.log('Rebex Fixes | Inizializzazione del modulo Rebex Fixes');
-    registerSettings();
+    console.log("Rebex Fixes | Impostazione di gioco registrata.");
 });
 
-Hooks.once('ready', () => {
-    if (game.user.isGM) {
-        console.log('Rebex Fixes | Modulo pronto e disponibile per l\'uso');
-    }
+Hooks.on('renderSettingsConfig', (app, html, data) => {
+    // Questa funzione serve per assicurarsi che la finestra di dialogo venga visualizzata correttamente
+    console.log("Rebex Fixes | Configurazione delle impostazioni resa.");
 });
-
-class RebexFixesApp extends FormApplication {
-    constructor() {
-        super();
-    }
-
-    static get defaultOptions() {
-        return mergeObject(super.defaultOptions, {
-            id: "rebex-fixes-app",
-            title: "Rebex Fixes",
-            template: "modules/rebex-fixes/templates/rebex-fixes.html",
-            width: 500,
-            height: "auto",
-            closeOnSubmit: true
-        });
-    }
-
-    async getData() {
-        return {
-            actors: game.actors.map(actor => actor.name),
-            compendiums: game.packs.map(pack => pack.collection)
-        };
-    }
-
-    async _updateObject(event, formData) {
-        console.log("Dati del form inviati:", formData);
-    }
-
-    activateListeners(html) {
-        super.activateListeners(html);
-
-        const fixTypeSelect = html.find('#fix-type');
-        const actorSelect = html.find('#actor-select');
-        const compendiumSelect = html.find('#compendium-select');
-        const fixActorButton = html.find('#fix-actor-button');
-        const fixConcentrationButton = html.find('#fix-concentration-button');
-
-        function updateFormVisibility() {
-            const fixType = fixTypeSelect.val();
-            if (fixType === "actor") {
-                actorSelect.show();
-                compendiumSelect.hide();
-            } else {
-                actorSelect.hide();
-                compendiumSelect.show();
-            }
-        }
-
-        updateFormVisibility();
-
-        fixTypeSelect.on('change', updateFormVisibility);
-
-        fixActorButton.on('click', async () => {
-            const actorName = actorSelect.val();
-            await CompendiumUtilities.updateActorItems(actorName);
-        });
-
-        fixConcentrationButton.on('click', async () => {
-            const fixType = fixTypeSelect.val();
-            if (fixType === "actor") {
-                const actorName = actorSelect.val();
-                await SpellConcentrationFixer.updateActorSpells(actorName);
-            } else if (fixType === "compendium") {
-                const compendiumName = compendiumSelect.val();
-                await SpellConcentrationFixer.updateCompendiumSpells(compendiumName);
-            }
-        });
-    }
-}
